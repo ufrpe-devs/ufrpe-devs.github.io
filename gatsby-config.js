14642324da9cc48d602e54d757a4b65fc4f70901
Italo Sousa <italusousa@gmail.com>
2021-09-26 19:59:31 -0300

    feat: Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/icon.png`,
       },
     },
   ],
