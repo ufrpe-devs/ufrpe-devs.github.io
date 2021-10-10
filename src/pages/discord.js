913b3c927d4d4968827cbe1c76d0b46ef320cd3a
Bruno Vasconcelos <brunohgv2@gmail.com>
2021-10-10 15:58:37 -0300

    Fix component padding

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -4,47 +4,67 @@ import { Flex, Heading, VStack } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import styled from '@emotion/styled';
 import Seo from '../components/seo';
-import discordLogo from '../assets/discord-logo.svg'
+import discordLogo from '../assets/discord-logo.svg';
 
 const Circle = styled.div`
   border-radius: 50%;
   width: 14px;
   height: 14px;
   margin-right: 8px;
-  background-color: ${props => props.color};
-`
+  background-color: ${(props) => props.color};
+`;
+
+const styles = {
+  content: {
+    paddingTop: '16px',
+    paddingBottom: '16px',
+  },
+  discordLogo: {
+    paddingRight: '8px',
+    paddingLeft: '8px',
+  },
+};
 
 const DiscordPage = () => {
-  const [discordInvitationLink, setDiscordInvitationLink] = React.useState()
-  const [discordPresenceCount, setDiscordPresenceCount] = React.useState()
+  const [discordInvitationLink, setDiscordInvitationLink] = React.useState();
+  const [discordPresenceCount, setDiscordPresenceCount] = React.useState();
 
   const fetchDiscordData = async () => {
-    const request = await fetch('https://discord.com/api/guilds/678334713919832100/widget.json')
-    const data = await request.json()
-    setDiscordInvitationLink(data.instant_invite)
-    setDiscordPresenceCount(data.presence_count)
-  }
+    const request = await fetch(
+      'https://discord.com/api/guilds/678334713919832100/widget.json'
+    );
+    const data = await request.json();
+    setDiscordInvitationLink(data.instant_invite);
+    setDiscordPresenceCount(data.presence_count);
+  };
 
   React.useEffect(() => {
     fetchDiscordData();
-  }, [])
+  }, []);
 
   return (
-  <>
-    <Seo title="Junte-se ao nosso Discord" />
-    <VStack direction="column" spacing={8}>
-      <Image src={discordLogo} alt="discord logo" />
-      <Heading as="h1" size="4xl" textAlign="center">
-        Junte-se ao nosso Discord
-      </Heading>
-      <Flex alignItems="center">
-        <Circle color="#58B286" /> {discordPresenceCount} Online
-      </Flex>
-      <a href={discordInvitationLink} target="_blank" rel="noreferrer">
-        <Button bg="brand.secondary" color="white">Continuar</Button>
-      </a>
-    </VStack>
-  </>
-)};
+    <>
+      <Seo title="Junte-se ao nosso Discord" />
+      <VStack direction="column" spacing={8} style={styles.content}>
+        <Image
+          src={discordLogo}
+          alt="discord logo"
+          style={styles.discordLogo}
+        />
+        <Heading as="h1" size="4xl" textAlign="center">
+          Junte-se ao nosso Discord
+        </Heading>
+        <Flex alignItems="center">
+          <Circle color="#58B286" /> {discordPresenceCount} Online
+        </Flex>
+        <a href={discordInvitationLink} target="_blank" rel="noreferrer">
+          <Button bg="brand.secondary" color="white">
+            Continuar
+          </Button>
+        </a>
+      </VStack>
+    </>
+  );
+};
 
 export default DiscordPage;
