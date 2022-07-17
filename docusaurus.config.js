0af40f71ab26b41be1a36488f31f4ff97936c931
Italo Sousa <italusousa@gmail.com>
2022-07-17 20:45:08 -0300

    chore: remove unused props

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,17 +35,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           routeBasePath: '/sobre',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: {},
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
